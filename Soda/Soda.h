5995591d2335461d311cd76801a6ff6b1f37a070
Qtechknow <quin@qtechknow.com>
2012-09-22 13:43:34 -0700

    Soda Library 2.0 Update

--- Soda/Soda.h
+++ Soda/Soda.h
@@ -1,7 +1,23 @@
+#ifndef Soda_h
+#define Soda_h
+
+#if ARDUINO >= 100     // Arduino 1.0 and 0023 compatible!
 #include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
 
-void Soda.pins(int a, int b, int c, int d, int e, int f, int g, int dp);
+class Soda_h
+{
+public:
+    void pins(int a, int b, int c, int d, int e, int f, int g, int dp);
+    void write(int number);
+    void setDecimalPoint(int digit, boolean decimalState);
 
-void Soda.setup();
+private:
+    int _a, _b, _c, _d, _e, _f, _g, _dp, _common;
+    int _number;
+    int _digit, boolean _decimalState;
+};
 
-void Soda.write(int number);
+#endif
